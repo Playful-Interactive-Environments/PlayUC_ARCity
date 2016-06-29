b0b6c615d32ccf1820a0aa437bf89c88fbb1f9dd
AlmostSonny <PSonnleitner@gmx.at>
2016-06-29 13:21:02 +0200

    Test

---
+++ CityAR/Assets/junction.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class junction : MonoBehaviour {
+
+	public GameObject[] connectingStreets;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+}
