1625a7e48cf1936f94426417e68df5f944ef3272
Georgi <Georgi.Kostov@fh-hagenberg.at>
2017-06-18 12:47:31 +0200

    new branch - bolt

---
+++ CityAR/Assets/BoltVersion/BoltPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Bolt;
+using UnityEngine;
+
+public class BoltPlayer : EntityBehaviour<IBoltPlayer>
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
